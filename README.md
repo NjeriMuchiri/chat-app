f3e0a550d4c1383d1a64133a186b98cfe1acc7c0
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-02-26 13:41:42 +0300

    launching the chat app

---
+++ README.md
@@ -0,0 +1,1 @@
+# chat-app use case of react chat engine
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+import './App.css'
+
+const App = () => {
+    return(
+        <ChatEngine 
+           height="100vh"
+           projectID="7e17b296-6565-4522-a9f7-b4016ee5e805"
+           userName="NjeriMuchiri"
+           userSecret="njesh@23"
+        />
+    )
+}
+
+
+export default App;
